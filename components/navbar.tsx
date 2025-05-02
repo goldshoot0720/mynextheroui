069a54cc6a044f58051d9925679a26f3b390d45f
goldshoot0720 <goldshoot0720@gmail.com>
2025-05-02 23:59:55 +0800

    mynextheroui init 6

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import {
   Navbar as HeroUINavbar,
   NavbarContent,
@@ -29,22 +31,27 @@ export const Navbar = () => {
   return (
     <HeroUINavbar maxWidth="xl" position="sticky">
       <NavbarContent className="basis-1/5 sm:basis-full" justify="start">
-        <NavbarBrand as="li" className="gap-3 max-w-fit"></NavbarBrand>
+        <NavbarBrand as="li" className="gap-3 max-w-fit">
+          <Logo />
+        </NavbarBrand>
+
         <ul className="hidden lg:flex gap-4 justify-start ml-2">
-          {siteConfig.navItems.map((item) => (
-            <NavbarItem key={item.href}>
-              <NextLink
-                className={clsx(
-                  linkStyles({ color: "foreground" }),
-                  "data-[active=true]:text-primary data-[active=true]:font-medium"
-                )}
-                color="foreground"
-                href={item.href}
-              >
-                {item.label}
-              </NextLink>
-            </NavbarItem>
-          ))}
+          {siteConfig.navItems
+            .filter(Boolean)
+            .map((item) => (
+              <NavbarItem key={item!.href}>
+                <NextLink
+                  className={clsx(
+                    linkStyles({ color: "foreground" }),
+                    "data-[active=true]:text-primary data-[active=true]:font-medium"
+                  )}
+                  color="foreground"
+                  href={item!.href}
+                >
+                  {item!.label}
+                </NextLink>
+              </NavbarItem>
+            ))}
         </ul>
       </NavbarContent>
 
@@ -55,23 +62,25 @@ export const Navbar = () => {
 
       <NavbarMenu>
         <div className="mx-4 mt-2 flex flex-col gap-2">
-          {siteConfig.navMenuItems.map((item, index) => (
-            <NavbarMenuItem key={`${item}-${index}`}>
-              <Link
-                color={
-                  index === 2
-                    ? "primary"
-                    : index === siteConfig.navMenuItems.length - 1
+          {siteConfig.navMenuItems
+            .filter(Boolean)
+            .map((item, index) => (
+              <NavbarMenuItem key={`${item!.href}-${index}`}>
+                <Link
+                  color={
+                    index === 2
+                      ? "primary"
+                      : index === siteConfig.navMenuItems.length - 1
                       ? "danger"
                       : "foreground"
-                }
-                href={item.href}
-                size="lg"
-              >
-                {item.label}
-              </Link>
-            </NavbarMenuItem>
-          ))}
+                  }
+                  href={item!.href}
+                  size="lg"
+                >
+                  {item!.label}
+                </Link>
+              </NavbarMenuItem>
+            ))}
         </div>
       </NavbarMenu>
     </HeroUINavbar>
